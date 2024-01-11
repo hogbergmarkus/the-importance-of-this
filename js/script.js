1ebccbad8914030eafa5865479eaa18a8339b079
Markus Högberg <m.o.hogberg@gmail.com>
2024-01-11 12:33:10 +0000

    Final lesson on this keyword

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-$('p').click(function () {
-    $('p').slideToggle('slow');
+$(document).ready(function () {
+    $('.box').on('click', function () {
+        /**
+         * When we click on an element that has
+         * a 'box' class, this element will be fired.
+         */
+        var classNames = $(this).attr('class').split(' ');
+        var boxClass = classNames[0];
+        var className = classNames[1];
+        if ($(this).css('background-color') == 'rgb(255, 0, 0)') {
+            // If this object is already red, turn it black
+            $('.' + className).css('background-color', '#000');
+        } else {
+            //Else turn all elements with a box class black
+            //and then change the color af all elements
+            //with the same class name as the clicked element ro red.
+            $('.' + boxClass).css('background-color', '#000');
+            $('.' + className).css('background-color', 'red');
+        }
+    });
 });
